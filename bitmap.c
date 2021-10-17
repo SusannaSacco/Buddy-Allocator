ad093b46dc3d107f3d39c6eb40158d8f7cca6bc2
SusannaSacco <90918371+SusannaSacco@users.noreply.github.com>
2021-10-17 21:44:10 +0200

    Update bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -8,3 +8,12 @@ void BitMap_new(BitMap* bitmap, int size, unsigned char* buffer){
 bitmap -> size = size;
 bitmap -> bits = size*8;
 }
+
+void BitMap_set(BitMap* bitmap, int n, int value) {
+    byte = i / 8;
+    assert(byte < bitmap->size);
+    if (value) {
+    bitmap->buffer[byte] |= 1 << (i & 7);
+} else {
+    bitmap->buffer[byte] &= ~(1 << (i & 7));
+}
