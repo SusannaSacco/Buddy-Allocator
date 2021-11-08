194f05565f18416d6fcf9486398e0e0bf5909a46
SusannaSacco <90918371+SusannaSacco@users.noreply.github.com>
2021-11-08 14:53:59 +0100

    Update bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -3,26 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void BitMap_new(BitMap* bitmap, int size, char* buffer){
-   bitmap -> buffer = buffer;
-   bitmap -> size = size;
-   bitmap -> bits = size*8;
+void BitMap_new(BitMap* bitmap, int bits, char* buffer){
+   bitmap->buffer = buffer;
+   bitmap->bits = bits;
+   if (bits % 8 == 0) {
+      bitmap->size = bits/8; }
+   else {
+      bitmap->size = bits/8 + 1;
+      }
 }
 
 void BitMap_set(BitMap* bitmap, int n, int value) {
-    byte = i / 8;
+    byte = n / 8;
     assert(byte < bitmap->size);
     if (value) {
-    bitmap->buffer[byte] |= 1 << (i & 7);
+    bitmap->buffer[byte] |= 1 << (n & 7);
 } else {
-    bitmap->buffer[byte] &= ~(1 << (i & 7));
+    bitmap->buffer[byte] &= ~(1 << (n & 7));
 }
 }
 
  int BitMap_get(BitMap* bitmap, int n) {
     int byte = n / 8;
     assert(byte < bitmap->size);
-    return bitmap->buffer[byte] & (1 << (i & 7)) ? 1 : 0;
+    return bitmap->buffer[byte] & (1 << (n & 7)) ? 1 : 0;
 }
 
 
