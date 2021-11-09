e8ef4435ac34b5882131a5ff96116fed90aa0cfc
SusannaSacco <90918371+SusannaSacco@users.noreply.github.com>
2021-11-09 18:19:19 +0100

    Update bitmap.h

--- bitmap.h
+++ bitmap.h
@@ -7,10 +7,17 @@ typedef struct {
   int bits;
 } BitMap;
 
+//crea una nuova bitmap con un buffer costituito da un array di char
 void BitMap_new(BitMap *bitmap, int bits, char* buffer);
 
+//imposta il bit n-esimo della bitmap al valore 0 o 1
 void BitMap_set(BitMap *bitmap, int n, int value);
 
-int BitMap_get(const BitMap *bitmap, int n);
+//restituisce il valore del n-esimo bit della bitmap
+int BitMap_get(BitMap *bitmap, int n);
 
+//stampa la bitmap come albero binario
 void BitMap_print(BitMap* bitmap);    
+
+//stampa la bitmap in una sola riga
+void BitMap_print2(BitMap* bitmap);
