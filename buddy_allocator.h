af14f320044fbd52353ef37ee718bae38ef7e4a2
SusannaSacco <90918371+SusannaSacco@users.noreply.github.com>
2021-11-09 18:20:29 +0100

    Update buddy_allocator.h

--- buddy_allocator.h
+++ buddy_allocator.h
@@ -8,17 +8,23 @@ typedef struct {
   int levels;
   int max_size;
   int block_size;
-  BitMap* bitmap;
+  BitMap bitmap;
   } BuddyAllocator;
 
+//crea un nuovo Buddy Allocator
 void BuddyAllocator_new(BuddyAllocator* a, char* memory,  int max_size,  int block_size, char* bitmap_buffer,  int bitmap_size);
 
+//individua il livello al quale dovrebbe trovarsi un blocco di size data
 int Find_level(BuddyAllocator* a, int size);
 
+//imposta ricorsivamente i figli del bit dato, al valore di 0 o 1 
 void Set_children(BuddyAllocator* a, int bit, int value);
 
+//imposta ricorsivamente il genitore del bit dato, al valore di 0 o 1
 void Set_parent(BuddyAllocator* a, int bit, int value);
 
+//alloca un blocco di memoria di size data
 void* BuddyAllocator_alloc(BuddyAllocator* a, int size);
 
-void BuddyAllocator_free(BuddyAllocator* a, void* ptr);
+//libera il blocco di memoria puntato da ptr
+void BuddyAllocator_free(BuddyAllocator* a, void* ptr); 
